4eaad3b639e824703e287719318eca5e8d6163f5
CodeCL207 <clovislhote@icloud.com>
2025-03-17 08:25:56 +0100

    premier test

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "CodeCL-Le jeu de la vie-1.0";
+const contentToCache = [
+    "Build/Exe WebGL.loader.js",
+    "Build/Exe WebGL.framework.js",
+    "Build/Exe WebGL.data",
+    "Build/Exe WebGL.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
